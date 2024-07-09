a11266e5900c3afa51d04699c44bb4aa88f3ba68
SethA <adkinsseth10@gmail.com>
2024-07-09 16:21:56 -0400

    Troubleshooting Adobe Embed API

--- fancy.js
+++ fancy.js
@@ -1,1 +1,12 @@
 /* This page handles the functionality for the page*/
+
+/* This is the functionality of the Adobe PDF viewer */
+document.addEventListener("adobe_dc_view_sdk.ready", function()
+        {
+           var adobeDCView = new AdobeDC.View({clientId: "463f26f5d78d49cba827efd6d4c7519a", divId: "adobe-dc-view"});
+           adobeDCView.previewFile(
+          {
+              content:  {location: {URL: "./Resume.pdf"}},
+              metaData: {fileName: "Resume.pdf"}
+          });
+        });
